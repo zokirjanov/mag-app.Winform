60aeb7c3f7b405f3bcfc4f4c5cfc03458708f646
shohruxbek <zokirjonovshohruxbek1016@gmail.com>
2023-03-09 10:21:09 +0500

    starter changes

--- src/mag-app.Winform/Windows/ProductForms/CategoriesForm.Designer.cs
+++ src/mag-app.Winform/Windows/ProductForms/CategoriesForm.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // CategoriesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(636, 530);
+            this.Name = "CategoriesForm";
             this.Text = "CategoriesForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
