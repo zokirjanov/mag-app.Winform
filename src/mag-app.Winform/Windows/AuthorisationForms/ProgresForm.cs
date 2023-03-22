eb266ee4f10c83d7328c606f50741f25da198176
shohruxbek <zokirjonovshohruxbek1016@gmail.com>
2023-03-22 17:40:23 +0500

    Some budg fixed

---
+++ src/mag-app.Winform/Windows/AuthorisationForms/ProgresForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mag_app.Winform.Windows.AuthorisationForms
+{
+    public partial class ProgresForm : Form
+    {
+        public ProgresForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
