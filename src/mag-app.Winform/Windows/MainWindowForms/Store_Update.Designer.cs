cb9d7cc4aabb1741943880d9753982b8bfd6a338
shohruxbek <zokirjonovshohruxbek1016@gmail.com>
2023-03-30 14:09:07 +0500

    Fixed Ui

---
+++ src/mag-app.Winform/Windows/MainWindowForms/Store_Update.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace mag_app.Winform.Windows
+{
+    partial class Store_Update
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.storeNameTb = new System.Windows.Forms.TextBox();
+            this.updateBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // storeNameTb
+            // 
+            this.storeNameTb.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.storeNameTb.Location = new System.Drawing.Point(32, 32);
+            this.storeNameTb.MaxLength = 30;
+            this.storeNameTb.Name = "storeNameTb";
+            this.storeNameTb.Size = new System.Drawing.Size(327, 29);
+            this.storeNameTb.TabIndex = 0;
+            // 
+            // updateBtn
+            // 
+            this.updateBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.updateBtn.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.updateBtn.Location = new System.Drawing.Point(69, 98);
+            this.updateBtn.Name = "updateBtn";
+            this.updateBtn.Size = new System.Drawing.Size(258, 39);
+            this.updateBtn.TabIndex = 2;
+            this.updateBtn.Text = "Редактировать";
+            this.updateBtn.UseVisualStyleBackColor = false;
+            this.updateBtn.Click += new System.EventHandler(this.updateBtn_Click);
+            // 
+            // UpdateForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 157);
+            this.Controls.Add(this.updateBtn);
+            this.Controls.Add(this.storeNameTb);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "UpdateForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "UpdateForm";
+            this.Load += new System.EventHandler(this.UpdateForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox storeNameTb;
+        private Button updateBtn;
+    }
+}
